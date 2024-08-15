8734f7eb16f4f2b7b91294d96c0a54b6579d9d0e
malwele2004 <malwele085@gmail.com>
2024-08-15 11:39:51 +0200

    Delete argument_list_parse directory

--- argument_list_parse/src/main/main.c
+++
@@ -1,9 +0,0 @@
-#define DEBUG
-#include "../../include/parse.h"
-
-int main(int argc, char** argv) {
-	arg_list a = arg_list_parse(argc, argv);
-	arg_list_debug(a);
-	arg_list_heap_deallocate(&a);
-	return 0;
-}
